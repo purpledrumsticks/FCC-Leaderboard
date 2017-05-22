46ff5b8426273e88abe7ea3e50e6b5453e852939
Corey Slavin <cslavin@cslavin-linux.corp.etranscor.com>
2017-05-22 08:30:18 -0400

    Refactor of Users component

--- src/Users.js
+++ src/Users.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Users extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <tbody className="user">
-        {this.props.users.map((user, i) => {
-          return (
-            <tr className="userTable" key={i + 1}>
-              <th>{i + 1}</th>
-              <th><img src={user.img}  alt="user"/>{user.username}</th>
-              <th className="points">{user.recent}</th>
-              <th className="points">{user.alltime}</th>
-            </tr>
-          )
-        })}
-      </tbody>
-    );
-  }
-}
-
-
+const Users = ({users}) =>
+    <tbody className="user">
+      {users.map((user, i) =>
+          <tr className="userTable" key={i + 1}>
+            <th>{i + 1}</th>
+            <th><img src={user.img}  alt="user"/>{user.username}</th>
+            <th className="points">{user.recent}</th>
+            <th className="points">{user.alltime}</th>
+          </tr>
+      )}
+    </tbody>
 
 export default Users;
